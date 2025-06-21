02bdb7fc3af0bf74456601cacc289d17e47e3f44
Richard Mannion <richardm@privateproperty.co.za>
2025-06-22 01:36:27 +0200

    fixed linux execute batch file

--- run-application.sh
+++ run-application.sh
@@ -1,2 +1,2 @@
-(cd game-engine && npm install && npm run build)
-(cd matching-game && npm install && npm run build)
+(cd game-engine && npm install && npm run build && npm link)
+(cd matching-game && npm install && npm link game-engine && npm run start)
